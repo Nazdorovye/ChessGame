9225ef71eef7e6e18c0fe8f1090c4eb439e6e1d6
Nazdorovye <naz-x@naz-xhm.zyxel.com>
2020-10-27 19:12:06 +0200

    tewmp

--- src/main/java/service/App.java
+++ src/main/java/service/App.java
@@ -8,18 +8,18 @@ import javafx.stage.Stage;
 
 public class App extends Application
 {
-    @Override
-    public void start(Stage stage) {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
-        stage.setScene(scene);
-        stage.show();
-    }
+  @Override
+  public void start(Stage stage) {
+    String javaVersion = System.getProperty("java.version");
+    String javafxVersion = System.getProperty("javafx.version");
+    Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + "");
+    Scene scene = new Scene(new StackPane(l), 640, 480);
+    stage.setScene(scene);
+    stage.show();
+  }
 
-    public static void main( String[] args )
-    {
-        launch();
-    }
+  public static void main(String[] args)
+  {
+    launch();
+  }
 }

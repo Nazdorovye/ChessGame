1a36331c30630cb2b15146dbf4903715e0cb37c3
Nazdorovye <cheshirebarsyk@gmail.com>
2021-01-07 20:14:36 +0200

    piece+

--- src/main/java/models/Piece.java
+++ src/main/java/models/Piece.java
@@ -1,5 +1,20 @@
 package models;
 
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
 public class Piece {
-  
+  protected ImageView visual;
+  // protected ArrayList<??> availableMoves;
+  protected boolean white;
+  protected byte x;
+  protected byte y;
+
+  private void OnMousePressed(MouseEvent e) {}
+  private void OnMouseReleased(MouseEvent e) {}
+
+  public Piece(ImageView pieceImage /*..*/) {
+    pieceImage.setOnMousePressed(this::OnMousePressed);
+    pieceImage.setOnMouseReleased(this::OnMouseReleased);
+}
 }

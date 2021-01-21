63ceed3c6b4a287e834fb890900d716c4cf89929
HotMakii <victorijakey@gmail.com>
2021-01-21 20:28:43 +0200

    changed to MoveMenuCtrl

---
+++ src/main/java/service/MoveMenuCtrl.java
@@ -0,0 +1,35 @@
+package service;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+
+public class MoveMenuCtrl {
+
+    @FXML AnchorPane moveMenuAPane;
+    @FXML Pane moveMenuPane;
+    @FXML VBox mainVbox;
+    @FXML HBox colorBox;
+    @FXML Label labelW;
+    @FXML Label labelB;
+    @FXML HBox timerBox;
+    @FXML Label timerW;
+    @FXML Label timerB;
+    @FXML ScrollPane scrollPane;
+    @FXML AnchorPane scrollApane;
+    @FXML TableView moveTable;
+    @FXML TableColumn tableW;
+    @FXML TableColumn tableB;
+    @FXML HBox buttonBox;
+    @FXML Button giveUp;
+    @FXML HBox spaceBox;
+    @FXML Button draw;
+    
+}

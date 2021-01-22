a43263493b58ba5e600633f16b53a625b6d19735
Nazdorovye <cheshirebarsyk@gmail.com>
2021-01-22 11:45:04 +0200

    + methods to mark & unmark available cells + game state checks onMousePressed

--- src/main/java/models/Piece.java
+++ src/main/java/models/Piece.java
@@ -1,8 +1,11 @@
 package models;
 
+import java.util.ArrayList;
+
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
-import service.StateGame;
+import service.Colour;
+import service.Game;
 
 public class Piece {
   protected class BoardCell {
@@ -19,28 +22,69 @@ public class Piece {
 
   protected Board board;
   protected ImageView visual;
-  // protected ArrayList<??> availableMoves;
-  protected boolean white;
+  protected ArrayList<BoardCell> availableMoves;
+  protected Colour colour;
   protected byte x;
   protected byte y;
 
-  private void OnMousePressed(MouseEvent e) {
-    // Check if player moves HIS piece
-    if (board.getGame().getGameState() == StateGame.SELECT_PIECE
-        && !board.isTurnCorrect(white)) {
-      System.out.println("Invalid turn!");
+  protected void markCells() {
+    board.cells[x][y].setPaneColor("#DDDDDD");
+    
+    for (BoardCell cell : availableMoves) {
+      board.cells[cell.x][cell.y].setPaneColor("#DDDDDD");
+    }
+  }
+
+  protected void unmarkCells() {
+    if (x % 2 == 0) {
+      board.cells[x][y].setPaneColor((y % 2 == 0) ? "#FFCE9E" : "#D18B47");
+    } else {
+      board.cells[x][y].setPaneColor((y % 2 != 0) ? "#FFCE9E" : "#D18B47");
+    }
+
+    for (BoardCell cell : availableMoves) {
+      if (cell.x % 2 == 0) {
+        board.cells[cell.x][cell.y].setPaneColor((cell.y % 2 == 0) ? "#FFCE9E" : "#D18B47");
+      } else {
+        board.cells[cell.x][cell.y].setPaneColor((cell.y % 2 != 0) ? "#FFCE9E" : "#D18B47");
+      }
+    }
+  }
+
+  protected void OnMousePressed(MouseEvent e) {
+    // Do nothing if in menu
+    if (board.getGame().getGameState().equals(Game.GameState.MENU)) {
+      System.out.println("No move in menu!");
+      e.consume();
+      return;
+    }
+
+    // Do nothing if wrong color
+    if (!colour.equals(board.getGame().getNextPlayer())) {
+      System.out.println("Wrong colour!");
+      e.consume();
+      return;
+    }
+
+    // Do nothing if already selected piece
+    if (!board.getGame().getTurnState().equals(Game.TurnState.SELECT_PIECE)) {
+      System.out.println("Select move!");
+      e.consume();
       return;
     }
   }
-  private void OnMouseReleased(MouseEvent e) {}
 
-  public Piece(Board board, boolean white, ImageView pieceImage, int x, int y) {
+  public Piece(Board board, Colour colour, ImageView pieceImage, int x, int y) {
     pieceImage.setOnMousePressed(this::OnMousePressed);
-    pieceImage.setOnMouseReleased(this::OnMouseReleased);
+
+    availableMoves = new ArrayList<BoardCell>();
 
     this.x = (byte)x;
     this.y = (byte)y;
     this.board = board;
-    this.white = white;
+    this.colour = colour;
+    this.visual = pieceImage;
   }
+
+  public void calcAvalableCells() {}
 }

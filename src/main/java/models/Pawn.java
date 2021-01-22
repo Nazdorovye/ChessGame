e8724f3deef862be1a59e4c5a22e2df12c146c80
Nazdorovye <cheshirebarsyk@gmail.com>
2021-01-22 11:41:02 +0200

    Pawn availble cells check complete refactor

--- src/main/java/models/Pawn.java
+++ src/main/java/models/Pawn.java
@@ -1,46 +1,56 @@
 package models;
 
-import java.util.ArrayList;
-
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import service.Colour;
+import service.Game.TurnState;
 
 public class Pawn extends Piece {
-  public Pawn(Board board, boolean white, ImageView pieceImage, int x, int y) {
-    super(board, white, pieceImage, x, y);
+  public Pawn(Board board, Colour colour, ImageView pieceImage, int x, int y) {
+    super(board, colour, pieceImage, x, y);
   }
 
+  @Override
+  protected void OnMousePressed(MouseEvent e) {
+    super.OnMousePressed(e);
+
+    // consumed in parent class
+    if (e.isConsumed()) return;
+
+    board.getGame().setTurnState(TurnState.SELECT_MOVE);
+    board.setNowSelected(this);
+    markCells();
+  }
+
+  @Override
   public void calcAvalableCells() {
-    int direction = white ? -1 : 1;
-    ArrayList<BoardCell> coords = new ArrayList<BoardCell>();
+    int direction = colour.direction();
+    availableMoves.clear();
     
-    // определяем доступные координаты        
-    coords.add(new BoardCell(x, (byte)(y - 1), false));
+    // cell next to pawn along direction
+    if ( board.cells[x][y + direction].getPiece() == null ) { 
+      availableMoves.add(new BoardCell(x, (byte)(y + direction), false));
+    }
     
-    if ( (white && y == 6) || (!white && y == 1) ) {
-        coords.add(new BoardCell(x, (byte)(y + (direction * 2)), false));
+    // en passant along direction
+    if ( (colour.white() && y == 6 && board.cells[x][5].getPiece() == null &&
+          board.cells[x][4].getPiece() == null ) || 
+
+         (colour.black() && y == 1 && board.cells[x][2].getPiece() == null &&
+          board.cells[x][3].getPiece() == null )) {
+      availableMoves.add(new BoardCell(x, (byte)(y + (direction * 2)), false));
     }        
     
-    if (x + 1 <= 7) { 
-        coords.add(new BoardCell((byte)(x + 1), (byte)(y + (direction)), false)); 
+    // kill right
+    if (x + 1 <= 7 && board.cells[x + 1][y + direction].getPiece() != null &&
+        !board.cells[x + 1][y + direction].getPiece().colour.equals(this.colour)) { 
+      availableMoves.add(new BoardCell((byte)(x + 1), (byte)(y + (direction)), false)); 
     }
     
-    if (x - 1 >= 0) {
-        coords.add(new BoardCell((byte)(x - 1), (byte)(y + (direction)), false));
-    }
-    
-    // откидываем занятые координаты
-    Piece temp;
-    for (int i = 0; i < coords.size(); i++) {
-      temp = board.cells[coords.get(i).x][coords.get(i).y].getPiece();
-
-      if (temp != null) {
-        if (temp.white == this.white) {
-          coords.remove(i--);
-          continue;
-        } 
-
-        coords.get(i).capture = true;
-      }
+    // kill left
+    if (x - 1 >= 0 && board.cells[x - 1][y + direction].getPiece() != null &&
+        !board.cells[x - 1][y + direction].getPiece().colour.equals(this.colour)) {
+      availableMoves.add(new BoardCell((byte)(x - 1), (byte)(y + (direction)), false));
     }
   }
 }

17a25dd6f148c8e078c67ca7b5d6fd40b6888b67
Nazdorovye <cheshirebarsyk@gmail.com>
2021-01-26 16:00:21 +0200

    renaming methods

--- src/main/java/models/Queen.java
+++ src/main/java/models/Queen.java
@@ -1,17 +1,16 @@
 package models;
 
 import service.Colour;
-import service.Move;
 
 public class Queen extends Piece {
-
-    public Queen(byte visualIdx, Colour colour, byte col, byte row) {
+  public Queen(byte visualIdx, Colour colour, byte col, byte row) {
     super(visualIdx, colour, col, row);
   }
 
   @Override
   public void calcAvalableCells(Board brd) {
+    moves.clear();
     Bishop.calculateDiagonalMoves(brd, this);
-    Rook.calculateVerticalAndHorizontalMoves(brd, this);
+    Rook.calculateLeveledMoves(brd, this);
   }
 }

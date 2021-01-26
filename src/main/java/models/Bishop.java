8be6ff611bb6c4ceb43f88c622bde75015562b34
Nazdorovye <cheshirebarsyk@gmail.com>
2021-01-26 15:55:26 +0200

    fixed logics

--- src/main/java/models/Bishop.java
+++ src/main/java/models/Bishop.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 
 import service.Colour;
 import service.Move;
+import service.Move.Type;
+
 public class Bishop extends Piece {
 
   public Bishop(byte visualIdx, Colour colour, byte col, byte row) {
@@ -11,15 +13,17 @@ public class Bishop extends Piece {
   }
 
   public static void calculateDiagonalMoves(Board brd, Piece callee) {
-    callee.getMoves().clear();
     if (callee.getStatus().taken()) return;
 
     boolean pieceFound = false;
+    boolean kingFound = false;
     Piece potPinnedPiece = null;
     ArrayList<Move> colCheckedMoves = new ArrayList<Move>();
 
     for (byte col_dir = -1; col_dir <= 1; col_dir += 2) {
       for (byte row_dir = -1; row_dir <= 1; row_dir += 2) {
+        pieceFound = false;
+        kingFound = false;
         colCheckedMoves.clear();
 
         for (byte col_dest = (byte)(callee.getCol() + col_dir), 
@@ -28,54 +32,80 @@ public class Bishop extends Piece {
           col_dest >= 0 && col_dest <= 7 && row_dest >= 0 && row_dest <= 7;
           col_dest += col_dir, row_dest += row_dir) {
         
-          if (pieceFound == true) {
+          // this iterates after rival king was found
+          if (kingFound && brd.getCells()[col_dest][row_dest].getPiece() == null) {
+            callee.moves.add(
+                new Move(Move.Type.CHECKED_DUMMY, callee.getCol(), callee.getRow(), col_dest, row_dest)
+            );
+            continue;
+          }
 
+          if (!pieceFound) {
+            // no first piece found, accumulate moves
             if (brd.getCells()[col_dest][row_dest].getPiece() == null) {
               colCheckedMoves.add(
-                new Move(Move.Type.CHECKED, callee.getCol(), callee.getRow(), col_dest, row_dest)
+                new Move(Move.Type.TRANSLATE, callee.getCol(), callee.getRow(), col_dest, row_dest)
               );
-              potPinnedPiece.setPinned(null);
-              continue; 
 
-            } else if (brd.getCells()[col_dest][row_dest].getPiece().getClass().equals(King.class)
-                  && !brd.getCells()[col_dest][row_dest].getPiece().colour.equals(callee.colour)) {
-              callee.getMoves().addAll(colCheckedMoves);
-              potPinnedPiece.setPinned(colCheckedMoves);
-            } 
+            // did first find ally piece - add accumulated moves to moves list and iterate next direction
+            } else if (brd.getCells()[col_dest][row_dest].getPiece().colour.equals(callee.colour)) {
+              brd.getCells()[col_dest][row_dest].getPiece().setStatus(Status.GUARDED);
+              break;
 
-            pieceFound = false;
-            break;
+            // first found piece is rival king 
+            } else if (brd.getCells()[col_dest][row_dest].getPiece().getClass().equals(King.class)) {
+              King king = (King)brd.getCells()[col_dest][row_dest].getPiece();
+              king.assignCheckedPiece(callee);
 
-          } else if (brd.getCells()[col_dest][row_dest].getPiece() == null) {
-            callee.getMoves().add(
-              new Move(Move.Type.TRANSLATE, callee.getCol(), callee.getRow(), col_dest, row_dest)
-            );
+              for (Move move : colCheckedMoves) move.type = Type.CHECKED;
 
-          } else if (brd.getCells()[col_dest][row_dest].getPiece().getClass().equals(King.class)
-                && !brd.getCells()[col_dest][row_dest].getPiece().colour.equals(callee.colour)) {
-            King king = (King)brd.getCells()[col_dest][row_dest].getPiece();
-            king.assignCheckedPiece(callee);
-            pieceFound = false;
-            break;
+              kingFound = true;
+              continue;
 
-          } else if (!brd.getCells()[col_dest][row_dest].getPiece().colour.equals(callee.colour)) {
-            callee.getMoves().add(
-              new Move(Move.Type.TAKE, callee.getCol(), callee.getRow(), col_dest, row_dest)
-            );
-            potPinnedPiece = brd.getCells()[col_dest][row_dest].getPiece();
-            pieceFound = true;
+            // first found piece is rival non-king 
+            } else {
+              callee.getMoves().addAll(colCheckedMoves); // anyways valid moves before piece
+              colCheckedMoves.clear(); // clear to collect pinned moves after piece
 
+              callee.getMoves().add(
+                new Move(Move.Type.TAKE, callee.getCol(), callee.getRow(), col_dest, row_dest)
+                );
+
+              potPinnedPiece = brd.getCells()[col_dest][row_dest].getPiece();
+              pieceFound = true;
+            }
+
+          // first rival piece found
           } else {
-            pieceFound = false;
-            break;
+
+            // empty cell after rival piece, no use
+            if (brd.getCells()[col_dest][row_dest].getPiece() == null) {
+              colCheckedMoves.add(
+                  new Move(Move.Type.CHECKED_DUMMY, callee.getCol(), callee.getRow(), col_dest, row_dest)
+              );
+
+            // rival king found after rival piece - pin rival piece
+            } else if (brd.getCells()[col_dest][row_dest].getPiece().getClass().equals(King.class)
+                && !brd.getCells()[col_dest][row_dest].getPiece().colour.equals(callee.colour)) {
+                
+              potPinnedPiece.setPinned(colCheckedMoves);
+              break;
+
+            // any non-king piece
+            } else {
+              break;
+            }
           }
         }
+
+        callee.moves.addAll(colCheckedMoves);
       }
     }
   }
 
   @Override
   public void calcAvalableCells(Board brd) {
+    moves.clear();
     calculateDiagonalMoves(brd, this);
   }    
 }

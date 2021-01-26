382fc87dd87c18d132d01bff2f5a14184dadb468
Nazdorovye <cheshirebarsyk@gmail.com>
2021-01-27 00:16:59 +0200

    formatting

--- src/main/java/service/PawnTransMenuCtrl.java
+++ src/main/java/service/PawnTransMenuCtrl.java
@@ -7,20 +7,18 @@ import javafx.scene.layout.Pane;
 
 public class PawnTransMenuCtrl {
 
-    @FXML Pane mainPawnPane;
-    @FXML GridPane gridP;
+  @FXML Pane mainPawnPane;
+  @FXML GridPane gridP;
 
 
-    @FXML private void initialize(){
+  @FXML private void initialize(){
 
-        AnchorPane.setBottomAnchor(mainPawnPane, .0);
-        AnchorPane.setTopAnchor(mainPawnPane, .0);
-        AnchorPane.setLeftAnchor(mainPawnPane, .0);
-        AnchorPane.setRightAnchor(mainPawnPane, .0);
+    AnchorPane.setBottomAnchor(mainPawnPane, .0);
+    AnchorPane.setTopAnchor(mainPawnPane, .0);
+    AnchorPane.setLeftAnchor(mainPawnPane, .0);
+    AnchorPane.setRightAnchor(mainPawnPane, .0);
 
-        gridP.prefWidthProperty().bind(mainPawnPane.widthProperty());
-        gridP.prefHeightProperty().bind(mainPawnPane.heightProperty());
-
-    }
-
+    gridP.prefWidthProperty().bind(mainPawnPane.widthProperty());
+    gridP.prefHeightProperty().bind(mainPawnPane.heightProperty());
+  }
 }

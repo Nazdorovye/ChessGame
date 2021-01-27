3f5fe761079654f38bbe3f7349187bbf441649c2
Nazdorovye <cheshirebarsyk@gmail.com>
2021-01-27 12:52:55 +0200

    added another colour toString

--- src/main/java/service/Colour.java
+++ src/main/java/service/Colour.java
@@ -6,4 +6,5 @@ public enum Colour {
   public byte direction() { return this.equals(Colour.WHITES) ? (byte)-1 : 1; }
   public boolean white() { return this.equals(Colour.WHITES) ? true : false; }
   public boolean black() { return this.equals(Colour.BLACKS) ? true : false; }
+  public String toStringRev() { return this.equals(Colour.BLACKS) ? WHITES.toString() : BLACKS.toString(); }
 }

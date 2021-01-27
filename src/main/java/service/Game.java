4b0bce115ba4eaaca91f8c03301377c9da4bd5c7
Nazdorovye <cheshirebarsyk@gmail.com>
2021-01-27 11:01:24 +0200

    +showChooseMenu +hideChooseMenu

--- src/main/java/service/Game.java
+++ src/main/java/service/Game.java
@@ -21,11 +21,13 @@ public class Game {
   }
 
   public final MainBoardCtrl boardCtrl;
+  public final PawnTransMenuCtrl transCtrl;
   private Board gameBoard;
   private Node transMenu;
   private GameState gameState;
   private TurnState turnState;
   private Colour playerTurn;
+  private double mouseX, mouseY;
 
   public void setNextPlayer() {
     playerTurn = Colour.values()[1 - playerTurn.ordinal()];
@@ -34,6 +36,13 @@ public class Game {
     setTurnState(TurnState.SELECT_PIECE);
     gameBoard.recalcMoves();
   }
+
+  public void setMouseCoords(double mouseX, double mouseY) {
+    this.mouseX = mouseX;
+    this.mouseY = mouseY;
+  }
+  public double getMouseX() { return mouseX; }
+  public double getMouseY() { return mouseY; }
   
   public void reset() {
     setTurnState(TurnState.SELECT_PIECE);
@@ -63,18 +72,18 @@ public class Game {
     boardCtrl.getChooserPane().getChildren().clear();
     boardCtrl.getChooserPane().getChildren().add(transMenu);
 
-    GridPane tmen = (GridPane)transMenu;
+    GridPane tmen = transCtrl.getGridPane();
     ImageView rk, bi, kn, qn;
 
     if (white) {
       rk = new ImageView(getClass().getResource("Chess_rlt60.png").toString());
       bi = new ImageView(getClass().getResource("Chess_blt60.png").toString());
-      kn = new ImageView(getClass().getResource("Chess_klt60.png").toString());
+      kn = new ImageView(getClass().getResource("Chess_nlt60.png").toString());
       qn = new ImageView(getClass().getResource("Chess_qlt60.png").toString());
     } else {
       rk = new ImageView(getClass().getResource("Chess_rdt60.png").toString());
       bi = new ImageView(getClass().getResource("Chess_bdt60.png").toString());
-      kn = new ImageView(getClass().getResource("Chess_kdt60.png").toString());
+      kn = new ImageView(getClass().getResource("Chess_ndt60.png").toString());
       qn = new ImageView(getClass().getResource("Chess_qdt60.png").toString());
     }
 
@@ -87,10 +96,26 @@ public class Game {
     tmen.add(bi, 1, 0);
     tmen.add(kn, 0, 1);
     tmen.add(qn, 1, 1);
+
+    double Y = mouseY;
+    if (Y < 10) {
+      Y = 10;
+    }
+
+    if (Y > boardCtrl.getAspectPane().getHeight() - boardCtrl.getChooserPane().getHeight() - 10) {
+      Y = boardCtrl.getAspectPane().getHeight() - boardCtrl.getChooserPane().getHeight() - 10;
+    }
+
+    boardCtrl.getChooserPane().relocate(mouseX, Y);
   }
 
-  public Game(MainBoardCtrl boardCtrl, Node transMenu) {
+  public void hideChooseMenu() {
+    boardCtrl.getChooserPane().getChildren().clear();
+  }
+
+  public Game(MainBoardCtrl boardCtrl, PawnTransMenuCtrl transCtrl, Node transMenu) {
     this.boardCtrl = boardCtrl;
+    this.transCtrl = transCtrl;
     this.transMenu = transMenu;
     gameBoard = new Board(this, boardCtrl);
     reset();
